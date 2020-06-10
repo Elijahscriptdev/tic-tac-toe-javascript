265477491a101db17f008e24db09a5e55787031a
elijahscriptdev <elijahobominuru@gmail.com>
2020-06-10 21:55:12 +0100

    add board

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,28 @@ const Player = (askedName, askedCoin) => {
   return { name, coin };
 };
 
+let winner = false;
+let draw = false;
+
+let gameBoard = [ ["one", "two", "three"], ["", "", ""], ["", "", ""] ];
+
+function restart() {
+// do something when cell are filled
+
+}
+
+
+// const winners = {
+//   row1: [1, 2, 3],
+//   row2: [4, 5, 6],
+//   row3: [7, 8, 9],
+//   row4: [1, 4, 7],
+//   row5: [2, 5, 8],
+//   row6: [3, 6, 9],
+//   row7: [1, 5, 9],
+//   row8: [3, 5, 7]
+// }
+
 const cells = document.querySelectorAll('[data-cell]');
 
 const player1 = Player('seth', 'X');
