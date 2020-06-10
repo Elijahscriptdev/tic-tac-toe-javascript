c7a0708dbee2312e168c9f579a5b139f75079fe6
sz16900 <sz16900@my.bristol.ac.uk>
2020-06-10 16:36:38 -0500

    updating board with players click

--- JS/index.js
+++ JS/index.js
@@ -4,16 +4,36 @@ const Player = (askedName, askedCoin) => {
   return { name, coin };
 };
 
-let winner = false;
-let draw = false;
+const Board = () => {
+  // Do not pass the board
+  let gameBoard = [
+    ["one", "two", "three"],
+    ["four", "five", "six"],
+    ["seven", "eight", "nine"],
+  ];
 
-let gameBoard = [ ["one", "two", "three"], ["", "", ""], ["", "", ""] ];
+  const getBoard = () => gameBoard;
+
+  const updateBoard = (position, coin) => {
+    for (let index = 0; index < gameBoard.length; index++) {
+      const pos = gameBoard[index].indexOf(position);
+      if (pos >= 0) {
+        gameBoard[index][pos] = coin;
+      }
+    }
+  };
+
+  // let winner = false;
+  // let draw = false;
+  return { getBoard, updateBoard };
+};
+
+// let coolBoard = Board();
+// console.log(coolBoard.getBoard());
 
 function restart() {
-// do something when cell are filled
-
+  // do something when cell are filled
 }
-
 
 // const winners = {
 //   row1: [1, 2, 3],
@@ -26,22 +46,25 @@ function restart() {
 //   row8: [3, 5, 7]
 // }
 
-const cells = document.querySelectorAll('[data-cell]');
+const cells = document.querySelectorAll("[data-cell]");
 
-const player1 = Player('seth', 'X');
-const player2 = Player('eli', 'O');
+const player1 = Player("seth", "X");
+const player2 = Player("eli", "O");
 let currentPlayer = player1.coin;
+let coolBoard = Board();
 
 function handleClick() {
   const currentCell = document.getElementById(this.id);
-  const choice = document.createElement('p');
-  choice.setAttribute('class', 'choice');
+  const choice = document.createElement("p");
+  choice.setAttribute("class", "choice");
   // REFACTOR THIS FOR ? :
   if (currentPlayer === player2) {
     choice.appendChild(document.createTextNode(player2.coin));
+    coolBoard.updateBoard(this.id, player2.coin);
     currentPlayer = player1;
   } else {
     choice.appendChild(document.createTextNode(player1.coin));
+    coolBoard.updateBoard(this.id, player1.coin);
     currentPlayer = player2;
   }
   //
@@ -49,5 +72,5 @@ function handleClick() {
 }
 
 cells.forEach((cell) => {
-  cell.addEventListener('click', handleClick, { once: true });
+  cell.addEventListener("click", handleClick, { once: true });
 });
